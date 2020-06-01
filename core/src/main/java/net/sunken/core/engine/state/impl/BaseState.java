8622cce40f9524d7f877015f688981ba5b81da17
Joseph <joseph42@live.co.uk>
2020-06-01 22:31:40 +0100

    Not needed

--- core/src/main/java/net/sunken/core/engine/state/impl/BaseState.java
+++ core/src/main/java/net/sunken/core/engine/state/impl/BaseState.java
@@ -42,19 +42,6 @@ public abstract class BaseState {
     //--- Called on state stop, before switching.
     public abstract void stop(BaseGameState next);
 
-    public boolean setState(@NonNull UUID uuid, @NonNull BasePlayerState newState) {
-        Optional<AbstractPlayer> abstractPlayerOptional = playerManager.get(uuid);
-
-        if (abstractPlayerOptional.isPresent()) {
-            CorePlayer corePlayer = (CorePlayer) abstractPlayerOptional.get();
-            corePlayer.setState(newState);
-
-            return true;
-        }
-
-        return false;
-    }
-
     public long getPlayingCount() {
         return playerManager.getOnlinePlayers().stream()
                 .map(abstractPlayer -> (CorePlayer) abstractPlayer)

0d07b6fbd3b5ba732134985f58ed5441ebf6ac38
Joseph <joseph42@live.co.uk>
2020-06-02 04:00:53 +0100

    4am btw

--- core/src/main/java/net/sunken/core/scoreboard/CustomScoreboardEntry.java
+++ core/src/main/java/net/sunken/core/scoreboard/CustomScoreboardEntry.java
@@ -29,9 +29,9 @@ public class CustomScoreboardEntry {
         Scoreboard scoreboard = customScoreboard.getScoreboard();
         Objective objective = customScoreboard.getObjective();
 
-        team = scoreboard.registerNewTeam("team-" + id.substring(0, 11));
-        score = objective.getScore("score-" + id.substring(0, 11));
-        team.addEntry("score-" + id.substring(0, 11));
+        team = scoreboard.registerNewTeam("team-" + (id.length() > 11 ? id.substring(0, 11) : id));
+        score = objective.getScore("score-" + (id.length() > 11 ? id.substring(0, 11) : id));
+        team.addEntry("score-" + (id.length() > 11 ? id.substring(0, 11) : id));
 
         updateTeam();
     }

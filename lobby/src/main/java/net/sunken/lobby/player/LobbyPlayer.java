2cdedf5e4d1bd68124fa3685b7744dda100c8f0c
Joseph <joseph42@live.co.uk>
2020-06-02 17:44:47 +0100

    Fix below

--- lobby/src/main/java/net/sunken/lobby/player/LobbyPlayer.java
+++ lobby/src/main/java/net/sunken/lobby/player/LobbyPlayer.java
@@ -51,14 +51,13 @@ public class LobbyPlayer extends CorePlayer {
                     + (serverMetadata.containsKey(ServerHelper.SERVER_METADATA_ID_KEY) ? serverMetadata.get(ServerHelper.SERVER_METADATA_ID_KEY) : "Pending.");
 
             CustomScoreboard customScoreboard = new CustomScoreboard(title);
-            customScoreboard.createEntry("Spacer1", ChatColor.WHITE + " ", 5);
+            customScoreboard.createEntry("Spacer1", ChatColor.WHITE + " ", 4);
 
-            customScoreboard.createEntry("RankTitle", ChatColor.WHITE + "Rank " + ColourUtil.fromColourCode(rank.getColourCode()) + "" + rank.getFriendlyName(), 4);
-            customScoreboard.createEntry("PlayersTitle", ChatColor.WHITE + "Players " + ChatColor.GREEN + serverManager.getTotalPlayersOnline(), 3);
+            customScoreboard.createEntry("RankTitle", ChatColor.WHITE + "Rank " + ColourUtil.fromColourCode(rank.getColourCode()) + "" + rank.getFriendlyName(), 3);
+            customScoreboard.createEntry("PlayersTitle", ChatColor.WHITE + "Players " + ChatColor.GREEN + serverManager.getTotalPlayersOnline(), 2);
 
-            customScoreboard.createEntry("Spacer3", ChatColor.RED + " ", 2);
-            customScoreboard.createEntry("ServerID", ChatColor.GRAY + pluginInform.getServer().getId(), 1);
-            customScoreboard.createEntry("URL", ChatColor.AQUA + "play.weapia.c" + ChatColor.AQUA + "om", 0);
+            customScoreboard.createEntry("Spacer3", ChatColor.RED + " ", 1);
+            customScoreboard.createEntry("URL", ChatColor.AQUA + "play.weapia.com", 0);
 
             customScoreboard.add(player);
             scoreboardRegistry.register(player.getUniqueId().toString(), customScoreboard);

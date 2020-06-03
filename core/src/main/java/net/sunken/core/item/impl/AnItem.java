26a9e52c2bac493ff9bd95d0b6d7314cf607f626
Joseph <joseph42@live.co.uk>
2020-06-03 19:49:38 +0100

    Fix stacking

--- core/src/main/java/net/sunken/core/item/impl/AnItem.java
+++ core/src/main/java/net/sunken/core/item/impl/AnItem.java
@@ -18,20 +18,24 @@ public class AnItem {
     private final AnItemAttributes attributes;
     @Getter
     private final AnItemListener listener;
+    private final boolean stack;
     private final ItemBuilder itemBuilder;
 
     public AnItem(@NonNull String id, @NonNull ItemBuilder itemBuilder, AnItemListener listener, boolean stack) {
         this.id = id;
         this.attributes = new AnItemAttributes();
         this.listener = listener;
+        this.stack = stack;
         this.itemBuilder = itemBuilder
                 .addNBTString(Constants.ITEM_NBT_KEY, this.id);
     }
 
     public ItemStack toItemStack() {
-        return itemBuilder
-                .addNBTString(Constants.ITEM_UUID_NBT_KEY, UUID.randomUUID().toString())
-                .make();
+        ItemBuilder itemBuilder = this.itemBuilder;
+        if (!this.stack) {
+            itemBuilder = itemBuilder.addNBTString(Constants.ITEM_UUID_NBT_KEY, UUID.randomUUID().toString());
+        }
+        return itemBuilder.make();
     }
 
 }

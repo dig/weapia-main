c90e01f21c060d707e286d02c89555fcf26d7fe0
Joseph <joseph42@live.co.uk>
2020-06-09 19:43:32 +0100

    Restart servers once built

--- master/src/main/java/net/sunken/master/Master.java
+++ master/src/main/java/net/sunken/master/Master.java
@@ -26,16 +26,6 @@ public class Master {
         masterFacetLoader.start();
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> this.handleGraceShutdown()));
-
-        /*
-        while (true) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-         */
     }
 
     private void handleGraceShutdown() {

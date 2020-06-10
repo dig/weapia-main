cebb916bb2d5b5b9d9b10aa3d358b49be150f68f
Joseph <joseph42@live.co.uk>
2020-06-10 15:29:59 +0100

    Bump

--- core/src/main/java/net/sunken/core/Core.java
+++ core/src/main/java/net/sunken/core/Core.java
@@ -42,7 +42,7 @@ public abstract class Core extends JavaPlugin {
         pluginFacetLoader.stop();
     }
 
-    public void handleGraceShutdown() {
+    private void handleGraceShutdown() {
         if (!shutdown) {
             shutdown = true;
 

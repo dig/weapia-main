27a8c2b737b6af6d77b55e654897e12b644a4411
Joseph <joseph42@live.co.uk>
2020-06-11 18:15:05 +0100

    Start ore wars

--- common/src/main/java/net/sunken/common/server/Game.java
+++ common/src/main/java/net/sunken/common/server/Game.java
@@ -8,7 +8,10 @@ import lombok.Getter;
 public enum Game {
 
     NONE ("None", 80, null, null),
-    ICE_RUNNER_SOLO ("Ice Runner (Solo)", 4, "respects/wep-infrastructure:icerunner-solo", "respects/wep-infrastructure-dev:icerunner-solo");
+    ICE_RUNNER_SOLO ("Ice Runner (Solo)", 4, "respects/wep-infrastructure:icerunner-solo", "respects/wep-infrastructure-dev:icerunner-solo"),
+
+    ORE_WARS_SOLO ("Ore Wars (Solo)", 6, "respects/wep-infrastructure:ore-wars-solo", "respects/wep-infrastructure-dev:ore-wars-solo"),
+    ORE_WARS_TEAM ("Ore Wars (Teams)", 12, "respects/wep-infrastructure:ore-wars-team", "respects/wep-infrastructure-dev:ore-wars-team");
 
     private String friendlyName;
     private int maxPlayers;

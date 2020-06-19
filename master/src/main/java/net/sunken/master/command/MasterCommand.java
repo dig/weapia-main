7451b98ed07bfa61661710bf01ca263889ee5e8b
Node <35534146+node-mc@users.noreply.github.com>
2020-06-19 02:04:14 +0100

    finish networkcommand

--- master/src/main/java/net/sunken/master/command/MasterCommand.java
+++ master/src/main/java/net/sunken/master/command/MasterCommand.java
@@ -3,7 +3,6 @@ package net.sunken.master.command;
 import net.sunken.common.command.impl.*;
 import net.sunken.common.inject.*;
 import net.sunken.common.player.*;
-import org.bukkit.command.*;
 
 import java.util.*;
 

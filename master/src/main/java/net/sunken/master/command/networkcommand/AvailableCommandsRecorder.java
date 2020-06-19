574434d309527990965fd897195b1c667edad302
Joseph <joseph42@live.co.uk>
2020-06-19 03:21:20 +0100

    Just wow

--- master/src/main/java/net/sunken/master/command/networkcommand/AvailableCommandsRecorder.java
+++ master/src/main/java/net/sunken/master/command/networkcommand/AvailableCommandsRecorder.java
@@ -28,8 +28,12 @@ public class AvailableCommandsRecorder implements Facet, Enableable {
             connection.del(NetworkCommandConstants.COMMAND_LIST_KEY);
 
             Set<BaseCommand> registeredCommands = commandRegistry.getRegisteredCommands();
+            log.info(String.format("COMMANDS BOI : %s", registeredCommands.toString()));
+
             Set<String> commandNames = new HashSet<>();
             registeredCommands.forEach(command -> commandNames.addAll(Arrays.asList(command.getCommand().aliases())));
+
+            log.info(String.format("COMMANDS BOI 2 : %s", commandNames.toString()));
 
             if (commandNames.size() > 0) {
                 connection.sadd(NetworkCommandConstants.COMMAND_LIST_KEY, commandNames.toArray(new String[commandNames.size()]));

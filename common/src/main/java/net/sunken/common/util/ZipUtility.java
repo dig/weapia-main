58fe876e548783c38c14a0e23cef70a860fe1a52
Joseph <joseph42@live.co.uk>
2020-06-20 22:24:58 +0100

    Add methods to extract zip

--- common/src/main/java/net/sunken/common/util/ZipUtility.java
+++ common/src/main/java/net/sunken/common/util/ZipUtility.java
@@ -1,14 +1,12 @@
 package net.sunken.common.util;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import lombok.experimental.UtilityClass;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 /**
  * This utility compresses a list of files to standard ZIP format file.
@@ -16,7 +14,9 @@ import java.util.zip.ZipOutputStream;
  * @author www.codejava.net
  *
  */
+@UtilityClass
 public class ZipUtility {
+
     /**
      * A constants for buffer size used to read/write data
      */
@@ -41,6 +41,7 @@ public class ZipUtility {
         zos.flush();
         zos.close();
     }
+
     /**
      * Compresses files represented in an array of paths
      * @param files a String array containing file paths
@@ -55,6 +56,7 @@ public class ZipUtility {
         }
         zip(listFiles, destZipFile);
     }
+
     /**
      * Adds a directory to the current zip output stream
      * @param folder the directory to be  added
@@ -83,6 +85,7 @@ public class ZipUtility {
             zos.closeEntry();
         }
     }
+
     /**
      * Adds a file to the current zip output stream
      * @param file the file to be added
@@ -103,5 +106,49 @@ public class ZipUtility {
             bytesRead += read;
         }
         zos.closeEntry();
+    }
+
+    /**
+     * Extracts a zip file specified by the zipFilePath to a directory specified by
+     * destDirectory (will be created if does not exists)
+     * @param zipFilePath
+     * @param destDirectory
+     * @throws IOException
+     */
+    public static void unzip(String zipFilePath, String destDirectory) throws IOException {
+        File destDir = new File(destDirectory);
+        if (!destDir.exists()) {
+            destDir.mkdir();
+        }
+        ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath));
+        ZipEntry entry = zipIn.getNextEntry();
+        while (entry != null) {
+            String filePath = destDirectory + File.separator + entry.getName();
+            if (!entry.isDirectory()) {
+                extractFile(zipIn, filePath);
+            } else {
+                File dir = new File(filePath);
+                dir.mkdir();
+            }
+            zipIn.closeEntry();
+            entry = zipIn.getNextEntry();
+        }
+        zipIn.close();
+    }
+
+    /**
+     * Extracts a zip entry (file entry)
+     * @param zipIn
+     * @param filePath
+     * @throws IOException
+     */
+    private static void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
+        byte[] bytesIn = new byte[BUFFER_SIZE];
+        int read = 0;
+        while ((read = zipIn.read(bytesIn)) != -1) {
+            bos.write(bytesIn, 0, read);
+        }
+        bos.close();
     }
 }

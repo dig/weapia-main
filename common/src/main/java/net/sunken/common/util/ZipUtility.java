0cff9cf7193c172ba155b14daeb60c52325133f7
Joseph <joseph42@live.co.uk>
2020-06-21 01:04:27 +0100

    Update buffer size

--- common/src/main/java/net/sunken/common/util/ZipUtility.java
+++ common/src/main/java/net/sunken/common/util/ZipUtility.java
@@ -134,7 +134,7 @@ public class ZipUtility {
                     Files.createDirectories(path);
                 } else {
                     try (OutputStream os = Files.newOutputStream(path)) {
-                        byte[] buffer = new byte[1024];
+                        byte[] buffer = new byte[BUFFER_SIZE];
                         int len;
                         while ((len = zis.read(buffer)) > 0) {
                             os.write(buffer, 0, len);

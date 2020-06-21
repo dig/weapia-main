d69c88fa4e307035cbebc9efb4cc945d0a1c5ade
Joseph <joseph42@live.co.uk>
2020-06-21 01:12:34 +0100

    Revert

--- common/src/main/java/net/sunken/common/util/ZipUtility.java
+++ common/src/main/java/net/sunken/common/util/ZipUtility.java
@@ -3,9 +3,6 @@ package net.sunken.common.util;
 import lombok.experimental.UtilityClass;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
@@ -119,31 +116,39 @@ public class ZipUtility {
      * @throws IOException
      */
     public static void unzip(String zipFilePath, String destDirectory) throws IOException {
-        Path root = Paths.get(destDirectory).normalize();
-        Path sourceZip = Paths.get(zipFilePath);
-        try (InputStream is = Files.newInputStream(sourceZip);
-             ZipInputStream zis = new ZipInputStream(is)) {
-            ZipEntry entry = zis.getNextEntry();
-            while (entry != null) {
-                Path path = root.resolve(entry.getName()).normalize();
-                if (!path.startsWith(root)) {
-                    throw new IOException("Invalid ZIP");
-                }
-
-                if (entry.isDirectory()) {
-                    Files.createDirectories(path);
-                } else {
-                    try (OutputStream os = Files.newOutputStream(path)) {
-                        byte[] buffer = new byte[BUFFER_SIZE];
-                        int len;
-                        while ((len = zis.read(buffer)) > 0) {
-                            os.write(buffer, 0, len);
-                        }
-                    }
-                }
-                entry = zis.getNextEntry();
-            }
-            zis.closeEntry();
+        File destDir = new File(destDirectory);
+        if (!destDir.exists()) {
+            destDir.mkdirs();
         }
+        ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath));
+        ZipEntry entry = zipIn.getNextEntry();
+        while (entry != null) {
+            String filePath = destDirectory + File.separator + entry.getName();
+            if (!entry.isDirectory()) {
+                extractFile(zipIn, filePath);
+            } else {
+                File dir = new File(filePath);
+                dir.mkdir();
+            }
+            zipIn.closeEntry();
+            entry = zipIn.getNextEntry();
+        }
+        zipIn.close();
+    }
+
+    /**
+     * Extracts a zip entry (file entry)
+     * @param zipIn
+     * @param filePath
+     * @throws IOException
+     */
+    private static void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
+        byte[] bytesIn = new byte[BUFFER_SIZE];
+        int read = 0;
+        while ((read = zipIn.read(bytesIn)) != -1) {
+            bos.write(bytesIn, 0, read);
+        }
+        bos.close();
     }
 }
